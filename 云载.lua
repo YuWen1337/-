121859957db9c6bbdfb3c6c8854a818828aca6bd
YuWen#5270 <107194279+YuWen1337@users.noreply.github.com>
2023-09-28 19:55:59 +0800

    Update 云载.lua

--- 云载.lua
+++ 云载.lua
@@ -1,16 +1,13 @@
 local _Enter_1,_Enter_2 = false,false
-local update_time = string.gsub(http.Get("https://raw.githubusercontent.com/YuWen1337/-/main/update_time.txt"), "\n", "");
-local use = string.gsub(http.Get("https://raw.githubusercontent.com/YuWen1337/-/main/use.txt"), "\n", "");
-local update_name
-local _lua_name
-if true then return end
+local update_time = http.Get("https://raw.githubusercontent.com/YuWen1337/-/main/update_time.txt")
+local use = http.Get("https://raw.githubusercontent.com/YuWen1337/-/main/use.txt")
 if use ~= "1" then return end
 local user_count = 0
 local WightScreen,HightScreen=draw.GetScreenSize()
 local _aimware_user_ = {{441151,1305773548},
 
 }
-local psd_window = gui.Window("","密码",WightScreen/2-500,HightScreen/2-500,440,380)
+local psd_window = gui.Window("","密码",WightScreen/2-500,HightScreen/2-500,160,150)
 local psd_editbox = gui.Editbox(psd_window, "", "输入密码")
 psd_window:SetActive(true)
 for k = 1,#_aimware_user_ do
@@ -37,16 +34,13 @@ if update_time ~= "2023年9月28日" then
 
 end
 local psd_button = gui.Button(psd_window, "确定", function ()
-    if _aimware_user_[user_count][2] == psd_editbox:GetValue() then
+    if _aimware_user_[user_count][2] == tostring(psd_editbox:GetValue()) then
         _Enter_2 = true
         psd_window:SetActive(false)
     end
 end)
 if not _Enter_2 then return end
-callbacks.Register("Draw",function()
-    print(1)
-
-    end)
+print(12222)
 
 
 
